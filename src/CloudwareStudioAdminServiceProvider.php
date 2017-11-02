31e133744adcfb94585ceabc76fc332610c0b512
Miguel Ángel Vinuesa Alonso <33325707+MiguelAngelVinuesaAlonso@users.noreply.github.com>
2017-11-02 17:50:25 +0100

    Add files via upload

---
+++ src/CloudwareStudioAdminServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Cloudware\Admin;
+
+use Illuminate\Support\ServiceProvider;
+
+class CloudwareStudioAdminServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/views' => base_path('resources/views/vendor/cloudware-studio/admin'),
+        ], 'views');
+
+        $this->publishes([
+            __DIR__.'/public' => public_path('vendor/cloudware-studio'),
+        ], 'public');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        include __DIR__ . '/http/routes.php';
+
+        $this->loadViewsFrom(base_path('resources/views/vendor/cloudware-studio/admin'), 'admin');
+    }
+}
---
+++ src/http/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+/***************************************************  ADMIN ROUTES ***************************************************/
+Route::get('admin', 'Cloudware\Admin\AdminController@index')->name('admin')->middleware('web');
+
